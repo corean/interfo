9c0912cdc6f9a6f5a378cae35c4712bcee937c82
corean <corean@corean.biz>
2023-01-09 17:42:40 +0900

    - 로고, 헤더 작업 및 동작 확인

---
+++ theme/interfo/about.php
@@ -0,0 +1,116 @@
+<!-- about.php -->
+<section class="about">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+            <h2>회사소개</h2>
+        </div>
+
+        <div class="row content">
+            <div class="col-lg-6">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore
+                    magna aliqua.
+                </p>
+                <ul>
+                    <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat
+                    </li>
+                    <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate
+                        velit
+                    </li>
+                    <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-6 pt-4 pt-lg-0">
+                <p>
+                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit
+                    in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <a href="#" class="btn-learn-more">Learn More</a>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="why-us section-bg">
+    <div class="container-fluid" data-aos="fade-up">
+
+        <div class="row">
+
+            <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
+
+                <div class="content">
+                    <h3>(주) 인터포는 고객성장과 가치창출을 먼저 생각합니다.</h3>
+                    <p>
+                        이를 위해서 (주)인터포에서는 5가지 기본원칙을 바탕으로 매출성장과 브랜드가치 성장을 위해 최선을 다하고 있습니다.
+                    </p>
+                </div>
+
+                <div class="accordion-list">
+                    <ul>
+                        <li>
+                            <a data-bs-toggle="collapse" class="collapse" data-bs-target="#accordion-list-1">
+                                <span>01</span>성실 <i class="bx bx-chevron-down icon-show"></i><i
+                                    class="bx bx-chevron-up icon-close"></i>
+                            </a>
+                            <div id="accordion-list-1" class="collapse show" data-bs-parent=".accordion-list">
+                                <p>(주) 인터포는 언제나 성실함을 기본으로 합니다.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a data-bs-toggle="collapse" data-bs-target="#accordion-list-2" class="collapsed">
+                                <span>02</span>창의 <i class="bx bx-chevron-down icon-show"></i><i
+                                    class="bx bx-chevron-up icon-close"></i>
+                            </a>
+                            <div id="accordion-list-2" class="collapse" data-bs-parent=".accordion-list">
+                                <p>(주) 인터포는 감각적이고 창의적인 생각으로 뭉쳤습니다.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a data-bs-toggle="collapse" data-bs-target="#accordion-list-3" class="collapsed">
+                                <span>03</span>열정 <i class="bx bx-chevron-down icon-show"></i><i
+                                    class="bx bx-chevron-up icon-close"></i>
+                            </a>
+                            <div id="accordion-list-3" class="collapse" data-bs-parent=".accordion-list">
+                                <p>(주) 인터포는 항상 넘치는 열정으로 임합니다.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a data-bs-toggle="collapse" data-bs-target="#accordion-list-4" class="collapsed">
+                                <span>04</span>도전 <i class="bx bx-chevron-down icon-show"></i><i
+                                    class="bx bx-chevron-up icon-close"></i>
+                            </a>
+                            <div id="accordion-list-4" class="collapse" data-bs-parent=".accordion-list">
+                                <p>(주) 인터포는 변화를 두려워하지 않습니다.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a data-bs-toggle="collapse" data-bs-target="#accordion-list-5" class="collapsed">
+                                <span>05</span>나눔 <i class="bx bx-chevron-down icon-show"></i><i
+                                    class="bx bx-chevron-up icon-close"></i>
+                            </a>
+                            <div id="accordion-list-5" class="collapse" data-bs-parent=".accordion-list">
+                                <p>(주) 인터포는 고객과 함께라는 마음과 나눔의 결과로 보답합니다.</p>
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+
+            </div>
+
+            <div class="col-lg-5 align-items-stretch order-1 order-lg-2 img"
+                 style='background-image: url("<?= ASSET_PATH ?>/img/why-us.png");' data-aos="zoom-in"
+                 data-aos-delay="150">&nbsp;
+            </div>
+        </div>
+
+    </div>
+</section>
