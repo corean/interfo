76a2b4c817f09b28d13cedd8a49390fcb5b9af5a
corean <corean@corean.biz>
2023-01-10 12:02:04 +0900

    - 회사소개 퍼블리싱

--- theme/interfo/about.php
+++ theme/interfo/about.php
@@ -7,34 +7,10 @@
         </div>
 
         <div class="row content">
-            <div class="col-lg-6">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                    labore et dolore
-                    magna aliqua.
-                </p>
-                <ul>
-                    <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat
-                    </li>
-                    <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate
-                        velit
-                    </li>
-                    <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat
-                    </li>
-                </ul>
-            </div>
-            <div class="col-lg-6 pt-4 pt-lg-0">
-                <p>
-                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit
-                    in voluptate
-                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-                <a href="#" class="btn-learn-more">Learn More</a>
+            <div class="col">
+                <h4>당신의 비즈니스를 더욱 빛나게 만드는 파트너!</h4>
+                <p>(주)인터포 핵심기술 개발에 대한 끊임없는 연구와 투자를 통해 모든이에게 도움이 되는 서비스,
+                    공공의 이익이 되는 서비스를 제공함으로써고객과 함께 성장해 나가겠습니다.</p>
             </div>
         </div>
     </div>
@@ -43,8 +19,7 @@
     <div class="container-fluid" data-aos="fade-up">
 
         <div class="row">
-
-            <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
+            <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch order-2 order-lg-1">
 
                 <div class="content">
                     <h3>(주) 인터포는 고객성장과 가치창출을 먼저 생각합니다.</h3>
@@ -58,7 +33,7 @@
                         <li>
                             <a data-bs-toggle="collapse" class="collapse" data-bs-target="#accordion-list-1">
                                 <span>01</span>성실 <i class="bx bx-chevron-down icon-show"></i><i
-                                    class="bx bx-chevron-up icon-close"></i>
+                                        class="bx bx-chevron-up icon-close"></i>
                             </a>
                             <div id="accordion-list-1" class="collapse show" data-bs-parent=".accordion-list">
                                 <p>(주) 인터포는 언제나 성실함을 기본으로 합니다.</p>
@@ -67,7 +42,7 @@
                         <li>
                             <a data-bs-toggle="collapse" data-bs-target="#accordion-list-2" class="collapsed">
                                 <span>02</span>창의 <i class="bx bx-chevron-down icon-show"></i><i
-                                    class="bx bx-chevron-up icon-close"></i>
+                                        class="bx bx-chevron-up icon-close"></i>
                             </a>
                             <div id="accordion-list-2" class="collapse" data-bs-parent=".accordion-list">
                                 <p>(주) 인터포는 감각적이고 창의적인 생각으로 뭉쳤습니다.</p>
@@ -76,7 +51,7 @@
                         <li>
                             <a data-bs-toggle="collapse" data-bs-target="#accordion-list-3" class="collapsed">
                                 <span>03</span>열정 <i class="bx bx-chevron-down icon-show"></i><i
-                                    class="bx bx-chevron-up icon-close"></i>
+                                        class="bx bx-chevron-up icon-close"></i>
                             </a>
                             <div id="accordion-list-3" class="collapse" data-bs-parent=".accordion-list">
                                 <p>(주) 인터포는 항상 넘치는 열정으로 임합니다.</p>
@@ -85,7 +60,7 @@
                         <li>
                             <a data-bs-toggle="collapse" data-bs-target="#accordion-list-4" class="collapsed">
                                 <span>04</span>도전 <i class="bx bx-chevron-down icon-show"></i><i
-                                    class="bx bx-chevron-up icon-close"></i>
+                                        class="bx bx-chevron-up icon-close"></i>
                             </a>
                             <div id="accordion-list-4" class="collapse" data-bs-parent=".accordion-list">
                                 <p>(주) 인터포는 변화를 두려워하지 않습니다.</p>
@@ -94,7 +69,7 @@
                         <li>
                             <a data-bs-toggle="collapse" data-bs-target="#accordion-list-5" class="collapsed">
                                 <span>05</span>나눔 <i class="bx bx-chevron-down icon-show"></i><i
-                                    class="bx bx-chevron-up icon-close"></i>
+                                        class="bx bx-chevron-up icon-close"></i>
                             </a>
                             <div id="accordion-list-5" class="collapse" data-bs-parent=".accordion-list">
                                 <p>(주) 인터포는 고객과 함께라는 마음과 나눔의 결과로 보답합니다.</p>
@@ -105,12 +80,44 @@
                 </div>
 
             </div>
-
             <div class="col-lg-5 align-items-stretch order-1 order-lg-2 img"
                  style='background-image: url("<?= ASSET_PATH ?>/img/why-us.png");' data-aos="zoom-in"
                  data-aos-delay="150">&nbsp;
             </div>
         </div>
 
+    </div>
+</section>
+<section class="partners">
+    <div class="container" data-aos="fade-up">
+        <h4 class="text-center mb-4">Our Partners</h4>
+        <img src="<?= ASSET_PATH ?>/img/about/partners.png" alt="" class="img-fluid">
+    </div>
+</section>
+<section class="catalog section-bg">
+    <div class="container">
+        <div class="col-lg-5 col-xl-6">
+            <h4>
+                (주)인터포 브로슈어
+            </h4>
+            <div class="mt-5 mb-5">
+                <p>
+                    당신의 비즈니스를 더욱 빛나게 만드는 파트너 (주)인터포는 핵심기술 개발에 대한
+                    끊임없는 연구와 투자를 통해 모든 이에게 도움이 되는 서비스와, 클라이언트
+                    비즈니스 환경에 맞는 컨설팅과 서비스를 지향하여 고객과 함께 성장해 나가고 있습니다.
+                </p>
+                <p>
+                    또한 풍부한 경험을 바탕으로 제작되는 홈페이지 및 편집물은 귀사의 새로운 비즈니스
+                    설계에 생명력을 불어넣어 드리며, 차별화된 커뮤니케이션 전략으로 귀사의 영업전략을 보다 강력하게 만들어 드립니다.
+                </p>
+            </div>
+            <a href="" class="d-inline-flex align-items-center">
+                <i class='bx bxs-file-pdf bx-tada bx-rotate-180 fs-3 me-2'></i>
+                <div>인터포 브로슈어 다운로드 (PDF)</div>
+            </a>
+        </div>
+        <div class="col-lg-5">
+
+        </div>
     </div>
 </section>
--- theme/interfo/content.php
+++ theme/interfo/content.php
@@ -38,216 +38,31 @@
 
 <body>
 
-<!-- ======= Header ======= -->
-<header id="header" class="fixed-top ">
-    <div class="container d-flex align-items-center">
-
-        <a href="index.html" class="logo me-auto"><img src="<?= ASSET_PATH ?>/img/interfo.svg" alt="" class="img-fluid"></a>
-
-        <nav id="navbar" class="navbar">
-            <ul>
-                <li><a class="nav-link scrollto" href="#about">회사소개</a></li>
-                <li><a class="nav-link scrollto" href="#services">Services</a></li>
-                <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
-                <li><a class="nav-link scrollto" href="#team">Team</a></li>
-                <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-                    <ul>
-                        <li><a href="#">Drop Down 1</a></li>
-                        <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                            <ul>
-                                <li><a href="#">Deep Drop Down 1</a></li>
-                                <li><a href="#">Deep Drop Down 2</a></li>
-                                <li><a href="#">Deep Drop Down 3</a></li>
-                                <li><a href="#">Deep Drop Down 4</a></li>
-                                <li><a href="#">Deep Drop Down 5</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Drop Down 2</a></li>
-                        <li><a href="#">Drop Down 3</a></li>
-                        <li><a href="#">Drop Down 4</a></li>
-                    </ul>
-                </li>
-                <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-            </ul>
-            <i class="bi bi-list mobile-nav-toggle"></i>
-        </nav><!-- .navbar -->
-
-    </div>
-</header><!-- End Header -->
+<header id="header" class="fixed-top">
+    <?php include_once(G5_THEME_PATH.'/header.php') ?>
+</header>
 
 <!-- ======= Hero Section ======= -->
 <section id="hero" class="d-flex align-items-center">
-
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1"
-                 data-aos="fade-up" data-aos-delay="200">
-                <h1>Better Solutions For Your Business</h1>
-                <h2>We are team of talented designers making websites with Bootstrap</h2>
-                <div class="d-flex justify-content-center justify-content-lg-start">
-                    <a href="#about" class="btn-get-started scrollto">Get Started</a>
-                    <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i
-                                class="bi bi-play-circle"></i><span>Watch Video</span></a>
-                </div>
-            </div>
-            <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-                <img src="<?= ASSET_PATH ?>/img/hero-img.png" class="img-fluid animated" alt="">
-            </div>
-        </div>
-    </div>
-
+    <?php include_once(G5_THEME_PATH.'/hero.php') ?>
 </section><!-- End Hero -->
 
 <main id="main">
 
-    <!-- ======= Clients Section ======= -->
-    <section id="clients" class="clients section-bg">
-        <div class="container">
-
-            <div class="row" data-aos="zoom-in">
-
-                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                    <img src="<?= ASSET_PATH ?>/img/clients/client-1.png" class="img-fluid" alt="">
-                </div>
-
-                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                    <img src="<?= ASSET_PATH ?>/img/clients/client-2.png" class="img-fluid" alt="">
-                </div>
-
-                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                    <img src="<?= ASSET_PATH ?>/img/clients/client-3.png" class="img-fluid" alt="">
-                </div>
-
-                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                    <img src="<?= ASSET_PATH ?>/img/clients/client-4.png" class="img-fluid" alt="">
-                </div>
-
-                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                    <img src="<?= ASSET_PATH ?>/img/clients/client-5.png" class="img-fluid" alt="">
-                </div>
-
-                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                    <img src="<?= ASSET_PATH ?>/img/clients/client-6.png" class="img-fluid" alt="">
-                </div>
-
-            </div>
-
-        </div>
-    </section><!-- End Cliens Section -->
+    <section class="business section-bg">
+        <?php include_once(G5_THEME_PATH.'/business.php') ?>
+    </section>
 
     <article id="about">
         <?php include_once(G5_THEME_PATH.'/about.php') ?>
     </article>
 
-    <!-- ======= Skills Section ======= -->
-    <section id="skills" class="skills">
-        <div class="container" data-aos="fade-up">
+    <article id="services">
+        <?php include_once(G5_THEME_PATH.'/service.php') ?>
+    </article>
 
-            <div class="row">
-                <div class="col-lg-6 d-flex align-items-center" data-aos="fade-right" data-aos-delay="100">
-                    <img src="<?= ASSET_PATH ?>/img/skills.png" class="img-fluid" alt="">
-                </div>
-                <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left" data-aos-delay="100">
-                    <h3>Voluptatem dignissimos provident quasi corporis voluptates</h3>
-                    <p class="fst-italic">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore
-                        magna aliqua.
-                    </p>
 
-                    <div class="skills-content">
 
-                        <div class="progress">
-                            <span class="skill">HTML <i class="val">100%</i></span>
-                            <div class="progress-bar-wrap">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0"
-                                     aria-valuemax="100"></div>
-                            </div>
-                        </div>
-
-                        <div class="progress">
-                            <span class="skill">CSS <i class="val">90%</i></span>
-                            <div class="progress-bar-wrap">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0"
-                                     aria-valuemax="100"></div>
-                            </div>
-                        </div>
-
-                        <div class="progress">
-                            <span class="skill">JavaScript <i class="val">75%</i></span>
-                            <div class="progress-bar-wrap">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0"
-                                     aria-valuemax="100"></div>
-                            </div>
-                        </div>
-
-                        <div class="progress">
-                            <span class="skill">Photoshop <i class="val">55%</i></span>
-                            <div class="progress-bar-wrap">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0"
-                                     aria-valuemax="100"></div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                </div>
-            </div>
-
-        </div>
-    </section><!-- End Skills Section -->
-
-    <!-- ======= Services Section ======= -->
-    <section id="services" class="services section-bg">
-        <div class="container" data-aos="fade-up">
-
-            <div class="section-title">
-                <h2>Services</h2>
-                <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
-                    consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
-                    fugiat sit in iste officiis commodi quidem hic quas.</p>
-            </div>
-
-            <div class="row">
-                <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-                    <div class="icon-box">
-                        <div class="icon"><i class="bx bxl-dribbble"></i></div>
-                        <h4><a href="">Lorem Ipsum</a></h4>
-                        <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
-                    </div>
-                </div>
-
-                <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in"
-                     data-aos-delay="200">
-                    <div class="icon-box">
-                        <div class="icon"><i class="bx bx-file"></i></div>
-                        <h4><a href="">Sed ut perspici</a></h4>
-                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-                    </div>
-                </div>
-
-                <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in"
-                     data-aos-delay="300">
-                    <div class="icon-box">
-                        <div class="icon"><i class="bx bx-tachometer"></i></div>
-                        <h4><a href="">Magni Dolores</a></h4>
-                        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
-                    </div>
-                </div>
-
-                <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in"
-                     data-aos-delay="400">
-                    <div class="icon-box">
-                        <div class="icon"><i class="bx bx-layer"></i></div>
-                        <h4><a href="">Nemo Enim</a></h4>
-                        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-    </section><!-- End Services Section -->
 
     <!-- ======= Cta Section ======= -->
     <section id="cta" class="cta">
